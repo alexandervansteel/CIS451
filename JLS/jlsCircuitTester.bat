35f2871e9d9ca80cd272beb444a7413c592b3b64
alexandervansteel <alexandervansteel@gmail.com>
2016-10-16 15:35:04 -0400

    updating from google drive

--- JLS/jlsCircuitTester.bat
+++ JLS/jlsCircuitTester.bat
@@ -1,26 +1,8 @@
+REM This script launches the JLS circuit tester, provided JLS.jar and 
+REM JLSCircuitTester.jar are in the same directory
+REM as this file.
 
-# This bash script launches the JLS circuit tester, provided JLS.jar and 
-# JLSCircuitTester.jar are in the same directory
-# as this file.
+REM %~dp0 returns argument 0 (the path to this batch file) as a
+REM directory (without the filename)
 
-# $0 contains the "name" of this script (the command typed at the
-# command line to launch it).  The dirname utility takes off the name
-# of the file and leaves the name of the directory containing the
-# script.  That directory name is then used to tell java where to find
-# JLS.
-
-# Notice that "." and $JLSCT_CLASSPATH are in the classpath.  This 
-# allows java to find any user-written OutputSets. 
-
-dir=`dirname $0`
-#echo "Dirname is " $dir
-
-# Make sure JLS.jar is around.
-if [ ! -e $dir/JLS.jar ]
-then
-	echo "JLS.jar is missing (or not in the right place)."
-	exit 1
-fi
-
-java -Xmx256m -cp $dir/JLS.jar:$dir/jlsCircuitTester.jar:.:$JLSCT_CLASSPATH -ea edu.gvsu.cis.kurmasz.jls.JLSCircuitTester "$@"
-exit $?  # causes the script to have the same exit value as the JVM.
+@java -Xmx256m -cp "%~dp0\JLS.jar";"%~dp0\jlsCircuitTester.jar";.;$JLSCT_CLASSPATH -ea C:\Users\Alexander\Desktop\JLS\JLSCircuitTester %*
